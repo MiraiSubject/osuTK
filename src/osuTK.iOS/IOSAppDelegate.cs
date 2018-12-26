31b72a680f11ee3c6ecf25c096d3e0baa4209030
Shivam Ramkisoen <s.r.v.ramkisoen@umail.leidenuniv.nl>
2018-12-26 13:45:08 +0100

    Add AppDelegate for OpenUrl handling (note: osu-framework Delegate needs to be inherited from this instead)

---
+++ src/osuTK.iOS/IOSAppDelegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UIKit;
+using Foundation;
+using osuTK.Input;
+using osuTK.Platform;
+
+namespace osuTK
+{
+    public abstract class IOSAppDelegate : UIApplicationDelegate
+    {
+
+       /* public abstract override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions); // Exposing FinishedLaunching for osu-framework
+
+
+        private readonly FileDropEventArgs FileDropArgs = new FileDropEventArgs();
+        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+        {
+           
+            var e = this.FileDropArgs;
+            FileDropEventArgs fileDropArgs = FileDropArgs;
+            fileDropArgs.FileName = url.Path;
+            
+            return true;
+        }*/
+    }
+}
