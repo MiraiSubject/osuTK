22ec129b951dbe218846a2dfde28ab2e5bcc7ba8
Shivam Ramkisoen <s.r.v.ramkisoen@umail.leidenuniv.nl>
2018-12-26 15:30:34 +0100

    Still investigating how filedrop works but this doesn't break

--- src/osuTK.iOS/IOSAppDelegate.cs
+++ src/osuTK.iOS/IOSAppDelegate.cs
@@ -4,23 +4,27 @@ using Foundation;
 using osuTK.Input;
 using osuTK.Platform;
 
-namespace osuTK
+namespace osuTK.iOS
 {
     public abstract class IOSAppDelegate : UIApplicationDelegate
     {
-
-       /* public abstract override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions); // Exposing FinishedLaunching for osu-framework
-
+       
+        public abstract override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions); // Exposing FinishedLaunching for osu-framework to handle
 
         private readonly FileDropEventArgs FileDropArgs = new FileDropEventArgs();
-        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+
+
+
+        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options) // File opening from external source. (Required Info.plist in game project)
         {
-           
-            var e = this.FileDropArgs;
-            FileDropEventArgs fileDropArgs = FileDropArgs;
-            fileDropArgs.FileName = url.Path;
-            
+            var e = FileDropArgs;
+            FileDropArgs.FileName = url.Path;
+            FileDrop(this, e);
             return true;
-        }*/
+        }
+        /// <summary>
+        /// Occurs whenever a file dropped on window;
+        /// </summary>
+        public event EventHandler<FileDropEventArgs> FileDrop = delegate { };
     }
 }
